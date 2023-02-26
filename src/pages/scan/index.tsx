61c7dbc4657e7be01a18c58d6a58559b0a6c342c
Jeremie Cyrille <jeremie.cyrille@icloud.com>
2023-02-26 13:54:15 +0100

    feat: add scanner librairy and scanner component

--- src/pages/scan/index.tsx
+++ src/pages/scan/index.tsx
@@ -1,7 +1,45 @@
-export default function Scanner () {
-  return (
-    <div>
+import React, { useState } from 'react'
+import {QrReader} from 'react-qr-reader'
+import { BalanceServices } from '@/Api/Services'
 
+
+export default function Scan () {
+  const [data, setData] = useState('No result')
+  const handleScan = (data: any) => {
+    if (data) {
+      setData(data)
+      BalanceServices.checkout(data)
+    }
+  }
+  const handleError = (err: any) => {
+    console.error(err)
+  }
+
+  const handleConstraints = () => {
+    
+    return {
+      facingMode: 'environment'
+    }
+  }
+  
+  return (
+    <div className="container max-w-2xl mx-auto mt-4">
+      <div className="flex">
+        <h1 className="flex-1 text-4xl">Scan</h1>
+      </div>
+      <div>
+        <QrReader
+          onResult={(result, error) => {
+            if (result) {
+              handleScan(result)
+            } else {
+              handleError(error)
+            }
+          } } 
+          constraints={handleConstraints()}
+        />
+        <p>{data}</p>
+      </div>
     </div>
   )
 }
