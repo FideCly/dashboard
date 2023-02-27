a4d463ae949471ab32417c5a5d8af988ab9109bf
jeremie cyrille <jeremie.cyrille@icloud.com>
2023-02-27 17:00:47 +0100

    feat : add prmotion select

--- src/pages/scan/index.tsx
+++ src/pages/scan/index.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
 import {QrReader} from 'react-qr-reader'
-import { BalanceServices } from '@/Api/Services'
+import { PromotionService, BalanceServices } from '@/Api/Services'
 
 
 export default function Scan () {
   const [data, setData] = useState('No result')
+  const [promotion, setPromotion] = useState<any>([])
+  
+  const [promotionID, setPromotionID] = useState<any>([])
 
+  //get promotion
+  React.useEffect(() => {
+    PromotionService.getPromotionsByShopId('1').then((response) => {
+      setPromotion(response.data)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+  }, [])
 
   const handleConstraints = () => {  
     return {
@@ -19,6 +31,19 @@ export default function Scan () {
         <h1 className="flex-1 text-4xl">Scan</h1>
       </div>
       <div>
+        {/* select with the promotion */}
+        <select className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline" name="promotion" id="promotion"
+          onChange={(e) => {
+            setPromotionID(e.target.value)
+          }}
+        >
+          {promotion.map((item: any) => {
+            return (
+              <option key={item.id} value={item.id} >{item.name}</option>
+            )
+          })}
+        </select>
+        
         <QrReader
           onResult={(result, error) => {
             if (result) {
