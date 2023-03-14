5c14b81051ee352dd0195dab2d9f1328fac6d229
jeremie cyrille <jeremie.cyrille@icloud.com>
2023-03-14 15:16:01 +0100

    fix: css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='flex'>
         <Navbar />
           <Main />
       </body>
